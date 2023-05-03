c3d3531f3ad898ad589a8c685ee5d466ad9a21d3
Jarosław Szewczyk <jaroslaw19szewczyk@gmail.com>
2023-05-03 12:30:04 +0200

    Creating multinomial and ordinal logistic regression best model test.

--- R_imputation.R
+++ R_imputation.R
@@ -241,15 +241,14 @@ saveRDS(imp_kNN_data_MNAR, "imp_kNN_data_MNAR.rds")
 
 #####Regression imputation#####
 
-######Choosing best linear model######
+######Defining model test dataset and variables######
 #Preparing data for test
 t <- data_MNAR_list[[14]][[3]] %>% 
-  select(-ID) %>% 
+  dplyr::select(-ID) %>% 
   as.data.frame() %>% 
   mutate_if(is.logical, as.factor)
 
-#Defining dependent and explanatory variables
-dependent_var <- t$tot_household_income
+#Defining explanatory variables
 explanatory_var <- c("tot_food_exp",
                      "bread_cereales_exp",
                      "rice_exp",
@@ -292,6 +291,10 @@ explanatory_var <- c("tot_food_exp",
                      "house_num_oven",
                      "house_num_boat",
                      "house_num_motorcycle_tricycle")
+
+######Choosing best linear regression model - var: tot_household_income######
+#Defining dependent variable
+dependent_var <- t$tot_household_income
 
 #Calculating AIC
 calculate_AIC <- function(vars) {
@@ -373,57 +376,9 @@ system.time(for (i in seq_along(data_MNAR_list)) {
 
 saveRDS(imp_lm_data_MNAR, "imp_lm_data_MNAR.rds")
 
-######Choosing best glm model - var: household_head_job######
-#Preparing data for test
-t <- data_MCAR_list[[1]][[3]] %>% 
-  select(-ID) %>% 
-  as.data.frame() %>% 
-  mutate_if(is.logical, as.factor)
-
-#Defining dependent and explanatory variables
+######Choosing best logistic regression model - var: household_head_job######
+#Defining dependent variable
 dependent_var <- t$household_head_job
-explanatory_var <- c("tot_food_exp",
-                     "bread_cereales_exp",
-                     "rice_exp",
-                     "meat_exp",
-                     "fish_exp",
-                     "fruit_exp",
-                     "vegetables_exp",
-                     "restaurant_hotels_exp",
-                     "alcohol_exp",
-                     "tobacco_exp",
-                     "clothing_exp",
-                     "housing_exp",
-                     "house_rent_exp",
-                     "medicare_exp",
-                     "transportation_exp",
-                     "communication_exp",
-                     "education_exp",
-                     "miscellaneous_goods_exp",
-                     "special_occasions_exp",
-                     "crop_farming_gardening_exp",
-                     "tot_income_enterpreneurial_act",
-                     "household_head_age",
-                     "num_family_member",
-                     "num_family_member_0_5",
-                     "num_family_member_5_17",
-                     "num_family_ember_employed",
-                     "house_area",
-                     "house_age",
-                     "house_num_bedrooms",
-                     "house_num_tv",
-                     "house_num_cd_dvd",
-                     "house_num_stereo",
-                     "house_num_refrigator",
-                     "num_washing_machine",
-                     "house_num_airconditioner",
-                     "house_num_car",
-                     "house_num_landline_phone",
-                     "house_num_smartphone",
-                     "house_num_computer",
-                     "house_num_oven",
-                     "house_num_boat",
-                     "house_num_motorcycle_tricycle")
 
 #Calculating AIC
 calculate_AIC <- function(vars) {
@@ -541,6 +496,62 @@ system.time(for (i in seq_along(data_MNAR_list)) {
 
 #Saving .rds file
 #saveRDS(imp_glm_data_MNAR, "imp_glm_data_MNAR.rds")
+
+######Choosing best multinomial logistic regression model - var: main_source_income######
+#Defining dependent variable
+dependent_var <- t$main_source_income
+
+#Calculating AIC
+calculate_AIC <- function(vars) {
+  formula <-
+    as.formula(paste("main_source_income ~ ", paste(vars, collapse = "+")))
+  model <- nnet::multinom(formula, data = t, trace = FALSE)
+  AIC(model)
+}
+
+#AIC function
+results_AIC <- c()
+for (i in 1:3) {
+  com <- combn(explanatory_var, i)
+  for (j in 1:ncol(com)) {
+    result <- calculate_AIC(com[, j])
+    results_AIC <- c(results_AIC, result)
+    names(results_AIC)[length(results_AIC)] <- paste(com[, j], collapse = "_")
+  }
+}
+
+#Choosing best model
+head(results_AIC[order(results_AIC)], 5)
+best_AIC <- min(results_AIC)
+results_AIC[which(results_AIC == best_AIC)]
+
+######Choosing best ordinal logistic regression model - var: house_type_wall######
+#Defining dependent variable
+dependent_var <- t$house_type_wall
+
+#Calculating AIC
+calculate_AIC <- function(vars) {
+  formula <-
+    as.formula(paste("house_type_wall ~ ", paste(vars, collapse = "+")))
+  model <- MASS::polr(formula, data = t, Hess = TRUE)
+  AIC(model)
+}
+
+#AIC function
+results_AIC <- c()
+for (i in 1:3) {
+  com <- combn(explanatory_var, i)
+  for (j in 1:ncol(com)) {
+    result <- calculate_AIC(com[, j])
+    results_AIC <- c(results_AIC, result)
+    names(results_AIC)[length(results_AIC)] <- paste(com[, j], collapse = "_")
+  }
+}
+
+#Choosing best model
+head(results_AIC[order(results_AIC)], 5)
+best_AIC <- min(results_AIC)
+results_AIC[which(results_AIC == best_AIC)]
 
 #####Random forest imputation#####
 
