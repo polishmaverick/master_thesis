edeba73b916b4d6c4b310f8dd9183f7cbad1aa14
Jarosław Szewczyk <jaroslaw19szewczyk@gmail.com>
2023-05-13 21:43:37 +0200

    Adding plots for hypothesis verification.

--- R_plot_hypothesis_verification.R
+++ R_plot_hypothesis_verification.R
@@ -1,48 +1,4 @@
-#####Plot example#####
-#X axis labels
-labels <- c(
-            "MCAR hot-deck",
-            "MAR hot-deck",
-            "MNAR hot-deck",
-            "MCAR kNN",
-            "MAR kNN",
-            "MNAR kNN",
-            "MCAR regresyjna",
-            "MAR regresyjna",
-            "MNAR regresyjna",
-            "MCAR random forest",
-            "MAR random forest",
-            "MNAR random forest",
-            "MCAR wielokrotna",
-            "MAR wielokrotna",
-            "MNAR wielokrotna")
-
-hypothesis <- expand.grid(X = labels, Y = seq(5, 70, 5))
-hypothesis <- transform(hypothesis, X = factor(X))
-hypothesis$Z <- runif(nrow(hypothesis)) * 100
-
-my_palette <- colorRampPalette(brewer.pal(n = 9, "Blues")[4:9])(100)
-
-hypothesis <- hypothesis %>%
-  left_join(all_data, by = c("X", "Y")) %>%
-  mutate(Z = ifelse(is.na(Z.y), Z.x, Z.y)) %>%
-  dplyr::select(X, Y, Z = Z.y)
-
-hypothesis$X <- as.factor(hypothesis$X)
-
-my_palette <- viridis(100, option = "mako", begin = 0.5, end = 0.0) # Tworzenie palety kolorów z palety mako
-
-levelplot(Z ~ X * Y,
-          data = hypothesis,
-          xlab = list(label = "Mechanizm powstania braków danych i metoda imputacji", fontsize = 20),
-          ylab = list(label = "Frakcja braków danych (w %)", fontsize = 20),
-          scales = list(x = list(rot = 45, cex = 1.4), y = list(cex = 1.4)),
-          col.regions = my_palette,
-          colorkey = list(labels = list(cex = 1.4),
-                          at = seq(0, 100, length.out = 100), 
-                          title = "% przypadków odrzucenia hipotezy H0"))
-
-######Jackknife median######
+######Oneway test######
 #Read .rds files
 readRDS("ow_hotdeck_MCAR.rds") -> ow_hotdeck_MCAR
 readRDS("ow_hotdeck_MAR.rds") -> ow_hotdeck_MAR
@@ -53,9 +9,9 @@ readRDS("ow_kNN_MNAR.rds") -> ow_kNN_MNAR
 readRDS("ow_reg_MCAR.rds") -> ow_reg_MCAR
 readRDS("ow_reg_MAR.rds") -> ow_reg_MAR
 readRDS("ow_reg_MNAR.rds") -> ow_reg_MNAR
-readRDS("ow_rf_MCAR.rds") -> ow_rf_MCAR
-readRDS("ow_rf_MAR.rds") -> ow_rf_MAR
-readRDS("ow_rf_MNAR.rds") -> ow_rf_MNAR
+readRDS("ow_regresyjna_MCAR.rds") -> ow_regresyjna_MCAR
+readRDS("ow_regresyjna_MAR.rds") -> ow_regresyjna_MAR
+readRDS("ow_regresyjna_MNAR.rds") -> ow_regresyjna_MNAR
 readRDS("ow_mul_MCAR.rds") -> ow_mul_MCAR
 readRDS("ow_mul_MAR.rds") -> ow_mul_MAR
 readRDS("ow_mul_MNAR.rds") -> ow_mul_MNAR
@@ -71,9 +27,9 @@ all_data <- rbind(
   transform(ow_reg_MCAR, method = "MCAR regresyjna"),
   transform(ow_reg_MAR, method = "MAR regresyjna"),
   transform(ow_reg_MNAR, method = "MNAR regresyjna"),
-  transform(ow_rf_MCAR, method = "MCAR random forest"),
-  transform(ow_rf_MAR, method = "MAR random forest"),
-  transform(ow_rf_MNAR, method = "MNAR random forest"),
+  transform(ow_regresyjna_MCAR, method = "MCAR random forest"),
+  transform(ow_regresyjna_MAR, method = "MAR random forest"),
+  transform(ow_regresyjna_MNAR, method = "MNAR random forest"),
   transform(ow_mul_MCAR, method = "MCAR wielokrotna"),
   transform(ow_mul_MAR, method = "MAR wielokrotna"),
   transform(ow_mul_MNAR, method = "MNAR wielokrotna")
@@ -81,6 +37,38 @@ all_data <- rbind(
 
 colnames(all_data) <- c("Y", "Z", "X")
 
+#Setting colors
+color_mapping <- c("MCAR hot-deck" = "#28192FFF",
+                   "MAR hot-deck" = "#28192FFF",
+                   "MNAR hot-deck" = "#28192FFF",
+                   "MCAR kNN" = "#40498EFF",
+                   "MAR kNN" = "#40498EFF",
+                   "MNAR kNN" = "#40498EFF",
+                   "MCAR regresyjna" = "#366A9FFF",
+                   "MAR regresyjna" = "#366A9FFF",
+                   "MNAR regresyjna" = "#366A9FFF",
+                   "MCAR random forest" = "#348AA6FF",
+                   "MAR random forest" = "#348AA6FF",
+                   "MNAR random forest" = "#348AA6FF",
+                   "MCAR wielokrotna" = "#38AAACFF",
+                   "MAR wielokrotna" = "#38AAACFF",
+                   "MNAR wielokrotna" = "#38AAACFF")
+
+#Plot
+ggplot(all_data, aes(x = Y, y = Z)) +
+  geom_point(aes(color = X), size = 3) +
+  labs(x = "Frakcja braków danych", y = "% przypadków odrzucenia hipotezy H0") +
+  facet_wrap(~ X, scales = "fixed", nrow = 5) +
+  theme_bw() +
+  theme(legend.position = "none",
+        plot.title = element_text(hjust = 0.5, size = 20),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 20, color = "black"),
+        axis.text.y = element_text(size = 20, color = "black"),
+        strip.text.x = element_text(size = 15),
+        axis.title = element_text(size = 25)) +
+  scale_color_manual(values = color_mapping) +
+  scale_x_continuous(breaks = seq(0, 70, 10),
+                     labels = function(x) paste0(x, "%"))
 
 ######Fisher test######
 #Read .rds files
@@ -93,9 +81,9 @@ readRDS("fisher_kNN_MNAR.rds") -> fisher_kNN_MNAR
 readRDS("fisher_reg_MCAR.rds") -> fisher_reg_MCAR
 readRDS("fisher_reg_MAR.rds") -> fisher_reg_MAR
 readRDS("fisher_reg_MNAR.rds") -> fisher_reg_MNAR
-readRDS("fisher_rf_MCAR.rds") -> fisher_rf_MCAR
-readRDS("fisher_rf_MAR.rds") -> fisher_rf_MAR
-readRDS("fisher_rf_MNAR.rds") -> fisher_rf_MNAR
+readRDS("fisher_regresyjna_MCAR.rds") -> fisher_regresyjna_MCAR
+readRDS("fisher_regresyjna_MAR.rds") -> fisher_regresyjna_MAR
+readRDS("fisher_regresyjna_MNAR.rds") -> fisher_regresyjna_MNAR
 readRDS("fisher_mul_MCAR.rds") -> fisher_mul_MCAR
 readRDS("fisher_mul_MAR.rds") -> fisher_mul_MAR
 readRDS("fisher_mul_MNAR.rds") -> fisher_mul_MNAR
@@ -111,9 +99,9 @@ all_data <- rbind(
   transform(fisher_reg_MCAR, method = "MCAR regresyjna"),
   transform(fisher_reg_MAR, method = "MAR regresyjna"),
   transform(fisher_reg_MNAR, method = "MNAR regresyjna"),
-  transform(fisher_rf_MCAR, method = "MCAR random forest"),
-  transform(fisher_rf_MAR, method = "MAR random forest"),
-  transform(fisher_rf_MNAR, method = "MNAR random forest"),
+  transform(fisher_regresyjna_MCAR, method = "MCAR random forest"),
+  transform(fisher_regresyjna_MAR, method = "MAR random forest"),
+  transform(fisher_regresyjna_MNAR, method = "MNAR random forest"),
   transform(fisher_mul_MCAR, method = "MCAR wielokrotna"),
   transform(fisher_mul_MAR, method = "MAR wielokrotna"),
   transform(fisher_mul_MNAR, method = "MNAR wielokrotna")
@@ -121,4 +109,35 @@ all_data <- rbind(
 
 colnames(all_data) <- c("Y", "Z", "X")
 
+#Setting colors
+color_mapping <- c("MCAR hot-deck" = "#28192FFF",
+                   "MAR hot-deck" = "#28192FFF",
+                   "MNAR hot-deck" = "#28192FFF",
+                   "MCAR kNN" = "#40498EFF",
+                   "MAR kNN" = "#40498EFF",
+                   "MNAR kNN" = "#40498EFF",
+                   "MCAR regresyjna" = "#366A9FFF",
+                   "MAR regresyjna" = "#366A9FFF",
+                   "MNAR regresyjna" = "#366A9FFF",
+                   "MCAR random forest" = "#348AA6FF",
+                   "MAR random forest" = "#348AA6FF",
+                   "MNAR random forest" = "#348AA6FF",
+                   "MCAR wielokrotna" = "#38AAACFF",
+                   "MAR wielokrotna" = "#38AAACFF",
+                   "MNAR wielokrotna" = "#38AAACFF")
 
+#Plot
+ggplot(all_data, aes(x = Y, y = Z)) +
+  geom_point(aes(color = X), size = 3) +
+  labs(x = "Frakcja braków danych", y = "% przypadków odrzucenia hipotezy H0") +
+  facet_wrap(~ X, scales = "fixed", nrow = 5) +
+  theme_bw() +
+  theme(legend.position = "none",
+        plot.title = element_text(hjust = 0.5, size = 20),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 20, color = "black"),
+        axis.text.y = element_text(size = 20, color = "black"),
+        strip.text.x = element_text(size = 15),
+        axis.title = element_text(size = 25)) +
+  scale_color_manual(values = color_mapping) +
+  scale_x_continuous(breaks = seq(0, 70, 10),
+                     labels = function(x) paste0(x, "%"))
